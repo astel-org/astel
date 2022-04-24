e13be117265f7726d0659420d57907844dc494b6
sumitkolhe <thesumitkolhe@gmail.com>
2022-04-24 21:02:48 +0530

    fix: theme provider

--- library/theme-provider/theme-provider.vue
+++ library/theme-provider/theme-provider.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { styleToString } from '../utils'
+import { commonVars, darkTheme, lightTheme, normalize } from './themes'
 
 type Theme = 'light-theme' | 'dark-theme'
 const theme: Theme[] = ['light-theme', 'dark-theme']
@@ -13,13 +15,27 @@ export default defineComponent({
       validator: (value: Theme) => Object.values(theme).includes(value),
     },
   },
+
+  setup() {
+    return {
+      normalize,
+      commonVars: `:root{ ${styleToString(commonVars)} }`,
+      lightStyle: `.light-theme{ ${styleToString(lightTheme)} }`,
+      darkStyle: `.dark-theme{ ${styleToString(darkTheme)} }`,
+    }
+  },
 })
 </script>
 
 <template>
-  <main :class="['common', $props.theme]"><slot></slot></main>
-</template>
+  <main>
+    <component :is="'style'">
+      {{ normalize }}
+      {{ commonVars }}
+      {{ lightStyle }}
+      {{ darkStyle }}
+    </component>
 
-<style lang="less" scoped>
-@import './style.less';
-</style>
+    <slot></slot>
+  </main>
+</template>
