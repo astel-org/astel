d7795e774ed0f023e31df4ed759dfaa93939f43f
sumitkolhe <thesumitkolhe@gmail.com>
2022-05-02 21:21:52 +0530

    fix: header github link

--- src/components/molecules/header.vue
+++ src/components/molecules/header.vue
@@ -35,22 +35,24 @@ export default defineComponent({
       </NuxtLink>
     </div>
 
-    <div class="flex border-l items-center justify-center flex-row w-14 hover:bg-gray-100">
-      <NuxtLink to="https://github.com/astel-org/astel" target="_blank">
-        <svg
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          shape-rendering="geometricPrecision"
-          class="w-5 h-auto stroke-gray-500"
-        >
-          <path
-            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
-          ></path>
-        </svg>
-      </NuxtLink>
-    </div>
+    <NuxtLink
+      to="https://github.com/astel-org/astel"
+      target="_blank"
+      class="flex border-l items-center justify-center flex-row w-14 hover:bg-gray-100"
+    >
+      <svg
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        shape-rendering="geometricPrecision"
+        class="w-5 h-auto stroke-gray-500"
+      >
+        <path
+          d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
+        ></path>
+      </svg>
+    </NuxtLink>
   </header>
 </template>
