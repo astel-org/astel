ed9ad87fa1ca90fcb651a5aede742d8202a00197
sumitkolhe <thesumitkolhe@gmail.com>
2022-05-02 21:21:30 +0530

    fix: export composables

--- library/index.ts
+++ library/index.ts
@@ -14,11 +14,12 @@ const install = (app: App) => {
   })
 }
 
-export default { install }
-
 export { default as Badge } from './badge'
 export { default as Button } from './button'
 export { default as Card } from './card'
 export { default as Error } from './error'
 export { default as Spinner } from './spinner'
 export { default as ThemeProvider } from './theme-provider'
+export * from './utils/composables'
+
+export default { install }
