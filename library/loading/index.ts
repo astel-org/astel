e03c4688bcd4ac16b95884c9e32cad326bb26761
sumitkolhe <thesumitkolhe@gmail.com>
2022-05-22 12:18:04 +0530

    feat(lib): add loading component

---
+++ library/loading/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '../utils'
+import _Loading from './loading.vue'
+
+export const Loading = withInstall(_Loading)
+
+export default Loading
---
+++ library/loading/loading.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Colors } from '../utils'
+
+export default defineComponent({
+  name: 'AsLoading',
+  props: {
+    type: {
+      type: String,
+      default: Colors.PRIMARY,
+      validator: (value: Colors) => Object.values(Colors).includes(value),
+    },
+    size: {
+      type: String || Number,
+      default: () => '6px',
+    },
+  },
+
+  setup(props) {
+    const rootClasses = computed(() => [{ [`as-loading--${props.type}`]: props.type }])
+    return { rootClasses }
+  },
+})
+</script>
+
+<template>
+  <div class="as-loading" :class="rootClasses" :style="`--as-loading-size: ${$props.size}`">
+    <div class="as-loading--spacer">
+      <slot></slot>
+    </div>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</template>
+
+<style lang="less">
+@import './style.less';
+</style>
