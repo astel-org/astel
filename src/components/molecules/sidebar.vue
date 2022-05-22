d6abe77edb6171a13995f6b49d6df7384a2cf56f
sumitkolhe <thesumitkolhe@gmail.com>
2022-05-22 12:21:25 +0530

    fix(docs): sidebar

--- src/components/molecules/sidebar.vue
+++ src/components/molecules/sidebar.vue
@@ -18,45 +18,23 @@ export default defineComponent({
     const sidebar = useSidebarStore()
 
     const sidebarItems: SidebarItems[] = [
-      {
-        title: 'Home',
-        link: '/',
-        children: [],
-      },
+      { title: 'Home', link: '/', children: [] },
       {
         title: 'Guide',
         link: '/guide',
         children: [
-          {
-            title: 'Introduction',
-            link: '',
-          },
-          {
-            title: 'Installation',
-            link: '',
-          },
-          {
-            title: 'Usage',
-            link: '',
-          },
+          { title: 'Introduction', link: '' },
+          { title: 'Installation', link: '' },
+          { title: 'Usage', link: '' },
         ],
       },
       {
         title: 'Components',
         link: '/components',
         children: [
-          {
-            title: 'Introduction',
-            link: '',
-          },
-          {
-            title: 'Installation',
-            link: '',
-          },
-          {
-            title: 'Usage',
-            link: '',
-          },
+          { title: 'Introduction', link: '' },
+          { title: 'Installation', link: '' },
+          { title: 'Usage', link: '' },
         ],
       },
     ]
@@ -71,32 +49,30 @@ export default defineComponent({
 
 <template>
   <Teleport to="body">
-    <Transition name="slide-fade">
-      <aside
-        v-if="sidebar.isVisible"
-        class="flex-col z-50 flex w-full sm:border-r sm:w-[320px] h-screen fixed bg-[color:var(--astel-background)]"
-      >
-        <header class="h-16 flex shrink-0 border-b">
-          <div class="flex items-center justify-between flex-1 px-6">
-            <span class="font-semibold"> Components </span>
-            <button
-              type="button"
-              title="Show Drawer"
-              class="sm:hidden"
-              @click="sidebar.toggleSidebar"
-            >
-              <X />
-            </button>
-          </div>
-        </header>
-        <Search />
-        <nav class="flex flex-col overflow-y-scroll h-full">
-          <div v-for="(item, index) in sidebarItems" :key="index">
-            <Dropdown :items="item.children" :title="`${item.title}`"> </Dropdown>
-          </div>
-        </nav>
-      </aside>
-    </Transition>
+    <aside
+      v-if="sidebar.isVisible"
+      class="flex-col z-50 flex w-full sm:border-r sm:w-[320px] h-screen fixed bg-[color:var(--astel-background)]"
+    >
+      <header class="h-16 flex shrink-0 border-b-[1px] border-[color:var(--accents-2)]">
+        <div class="flex items-center justify-between flex-1 px-6">
+          <span class="font-semibold"> Components </span>
+          <button
+            type="button"
+            title="Show Drawer"
+            class="sm:hidden"
+            @click="sidebar.toggleSidebar"
+          >
+            <X />
+          </button>
+        </div>
+      </header>
+      <Search />
+      <nav class="flex flex-col overflow-y-scroll h-full">
+        <div v-for="(item, index) in sidebarItems" :key="index">
+          <Dropdown :items="item.children" :title="`${item.title}`"> </Dropdown>
+        </div>
+      </nav>
+    </aside>
   </Teleport>
 </template>
 
