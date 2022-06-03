0fad29a321c5b49906f2cc9bb34f7f78cf290b85
sumitkolhe <thesumitkolhe@gmail.com>
2022-06-03 20:54:30 +0530

    feat: docs init pages

---
+++ src/pages/guide/[...slug].vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<template>
+  <div>
+    <ContentDoc class="prose prose-gray dark:prose-invert max-w-none">
+      <template #not-found>
+        <h1 class="text-2xl">Page not found</h1>
+      </template>
+    </ContentDoc>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -39,42 +39,33 @@ export default defineComponent({
 </script>
 
 <template>
-  <section class="flex flex-col md:h-screen w-screen justify-center mx-auto">
-    <div class="flex-row mt-44 md:mt-0 mb-12">
-      <div class="text-center w-full">
-        <h1 class="text-6xl font-bold">Astel</h1>
-        <p class="text-2xl mt-4 text-[color:var(--accents-5)] font-medium">
-          A sleek and minimal Vue 3 UI component library
-        </p>
-      </div>
-      <div class="flex items-center justify-center mt-10">
-        <NuxtLink to="/components/button">
-          <as-button size="large" type="success" variant="shadow">Get Started</as-button>
-        </NuxtLink>
-      </div>
+  <section class="flex flex-col justify-center items-center">
+    <div class="text-center w-full mt-44">
+      <h1 class="text-6xl font-bold">Astel</h1>
+      <p class="text-2xl mt-4 text-[color:var(--accents-5)] font-medium">
+        A sleek and minimal Vue 3 UI component library
+      </p>
+    </div>
+    <div class="flex items-center justify-center mt-10">
+      <NuxtLink to="/guide">
+        <as-button size="large" type="success" variant="shadow">Get Started</as-button>
+      </NuxtLink>
+    </div>
 
-      <div
-        class="text-center mt-32 px-6 lg:px-0 flex flex-col md:flex-row md:max-w-5xl mx-auto w-full gap-8"
-      >
-        <NuxtLink
-          v-for="(feature, index) in features"
-          :key="index"
-          :to="feature.link"
-          class="flex-1"
-        >
-          <as-card hoverable shadow>
-            <div class="flex flex-row items-center p-2">
-              <span class="border-[1px] border-[color:var(--accents-2)] rounded-full px-2 py-3">
-                <component :is="feature.icon" class="mx-1" />
-              </span>
-              <p class="text-xl font-semibold mx-3">{{ feature.title }}</p>
-            </div>
-            <p class="text-left p-2 text-[color:var(--accents-6)]">
-              {{ feature.description }}
-            </p>
-          </as-card>
-        </NuxtLink>
-      </div>
+    <div class="text-center mt-32 px-6 flex flex-row w-full gap-8">
+      <NuxtLink v-for="(feature, index) in features" :key="index" :to="feature.link" class="flex-1">
+        <as-card hoverable shadow>
+          <div class="flex flex-row items-center p-2">
+            <span class="border-[1px] border-[color:var(--accents-2)] rounded-full px-2 py-3">
+              <component :is="feature.icon" class="mx-1" />
+            </span>
+            <p class="text-xl font-semibold mx-3">{{ feature.title }}</p>
+          </div>
+          <p class="text-left p-2 text-[color:var(--accents-6)]">
+            {{ feature.description }}
+          </p>
+        </as-card>
+      </NuxtLink>
     </div>
   </section>
 </template>
