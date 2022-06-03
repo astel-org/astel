b00a89cb2b5be73089f5ba3c9a265c2d65f73156
sumitkolhe <thesumitkolhe@gmail.com>
2022-06-03 20:52:15 +0530

    feat: default layout

---
+++ default.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { computed, defineComponent } from 'vue'
+import Header from '../components/molecules/header.vue'
+import { useRoute } from '#imports'
+import Drawer from '~~/src/components/molecules/drawer.vue'
+
+const route = useRoute()
+
+const path = computed(() => route.path)
+
+return { path }
+</script>
+
+<template>
+  <ClientOnly>
+    <as-theme-provider>
+      <Header />
+      <div class="flex flex-col w-screen">
+        <aside class="bg-red-500">
+          <Drawer />
+        </aside>
+        <section class="flex flex-row">
+          <slot />
+        </section>
+      </div>
+    </as-theme-provider>
+  </ClientOnly>
+</template>
